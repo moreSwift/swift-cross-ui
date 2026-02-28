97a42fb9f3d0935732b79256d270214e2ce51ac7
stackotter <stackotter@stackotter.dev>
2026-02-28 16:20:46 +1000

    AndroidBackend: Implement TextField

--- Sources/AndroidBackend/Kotlin/dev/swiftcrossui/androidbackend/SwiftObject.kt
+++ Sources/AndroidBackend/Kotlin/dev/swiftcrossui/androidbackend/SwiftObject.kt
@@ -1,6 +1,5 @@
 package dev.swiftcrossui.androidbackend
 
-// Taken from https://github.com/PureSwift/Android/blob/e980a12f6d7236bed32ff687b40dae2366ac8e91/Demo/app/src/main/java/com/pureswift/swiftandroid/SwiftObject.kt#L3-L4
 /// Swift object retained by JVM. We have to support both 32-bit and 64-bit
 /// systems, so we store the pointers as longs.
 class SwiftObject(val pointerValue: Long) {
