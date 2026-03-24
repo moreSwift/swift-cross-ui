7357f0e852dd426eba7b75acc2d03b4cdbc51989
stackotter <stackotter@stackotter.dev>
2026-03-25 01:04:52 +1000

    Move StackLayoutCache and LayoutPriorityGroup into own files

--- Sources/SwiftCrossUI/Layout/LayoutSystem.swift
+++ Sources/SwiftCrossUI/Layout/LayoutSystem.swift
@@ -102,17 +102,6 @@ public enum LayoutSystem {
         }
     }
 
-    /// The subset of a group's children with a particular layout priority.
-    struct LayoutPriorityGroup {
-        /// The view's children, referenced by index, using the index of the
-        /// child based on the visual order of the stack's children (rather
-        /// than the flexibility-based layout order). The children are in
-        /// order of increasing flexibility.
-        var children: ArraySlice<Int>
-        /// The layout priority of all children within this subset.
-        var priority: Double
-    }
-
     /// - Parameter inheritStackLayoutParticipation: If `true`, the stack layout
     ///   will have ``ViewSize/participateInStackLayoutsWhenEmpty`` set to `true`
     ///   if all of its children have it set to true. This allows views such as
