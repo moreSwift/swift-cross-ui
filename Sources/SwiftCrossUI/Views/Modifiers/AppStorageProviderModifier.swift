dcd5e8e503bc207c9450da1afc05c12dce80d242
Kaleb A. Ascevich <github.sheath428@passmail.com>
2026-04-04 14:32:04 -0400

    make `appStorageProvider(_:)` work on scenes

--- Sources/SwiftCrossUI/Views/Modifiers/AppStorageProviderModifier.swift
+++ Sources/SwiftCrossUI/Views/Modifiers/AppStorageProviderModifier.swift
@@ -5,8 +5,17 @@ extension View {
     /// - Parameter provider: The app storage provider to use in this
     ///   view and its subviews.
     public func appStorageProvider(_ provider: some AppStorageProvider) -> some View {
-        EnvironmentModifier(self) { environment in
-            environment.with(\.appStorageProvider, provider)
-        }
+        environment(\.appStorageProvider, provider)
+    }
+}
+
+extension Scene {
+    /// Sets the app storage provider used to persist state annotated
+    /// with ``AppStorage``.
+    ///
+    /// - Parameter provider: The app storage provider to use in this
+    ///   scene and its subviews.
+    public func appStorageProvider(_ provider: some AppStorageProvider) -> some Scene {
+        environment(\.appStorageProvider, provider)
     }
 }
