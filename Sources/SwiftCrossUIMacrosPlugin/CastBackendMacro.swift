6b4e0b48f12a96cc798c09dc8ae4bbc2e0130b68
Kaleb A. Ascevich <github.sheath428@passmail.com>
2026-04-10 13:36:24 -0400

    tweak the macro implementation

--- Sources/SwiftCrossUIMacrosPlugin/CastBackendMacro.swift
+++ Sources/SwiftCrossUIMacrosPlugin/CastBackendMacro.swift
@@ -6,109 +6,105 @@ import SwiftSyntaxMacros
 
 public struct CastBackendMacro: BodyMacro {
     public static func expansion(
-        of node: AttributeSyntax,
-        providingBodyFor declaration: some DeclSyntaxProtocol & WithOptionalCodeBlockSyntax,
+        of attribute: AttributeSyntax,
+        providingBodyFor decl: some DeclSyntaxProtocol & WithOptionalCodeBlockSyntax,
         in context: some MacroExpansionContext
     ) throws -> [CodeBlockItemSyntax] {
+        // MARK: Checks
+
         // Get the single generic argument.
         guard
-            let genericArgs = Attribute(node).name.genericArguments,
-            let newBackendType = genericArgs.first?._baseSyntax
+            let newBackend = Attribute(attribute).name
+                .genericArguments?.first?._baseSyntax
         else {
-            throw MacroError("@CastBackend macro expects a single type parameter")
+            throw MacroError(
+                "@CastBackend macro expects a single type parameter"
+            )
         }
 
         // Make sure this is a function and it has a `backend` parameter.
         guard
-            let signature = declaration.as(FunctionDeclSyntax.self)?.signature,
+            let signature = decl.as(FunctionDeclSyntax.self)?.signature,
             let backendParameter = signature.parameterClause.parameters.first(where: {
                 ($0.secondName ?? $0.firstName).trimmedDescription == "backend"
             })
         else {
-            throw MacroError("@CastBackend macro expects a function with a `backend` argument")
+            throw MacroError(
+                "@CastBackend macro expects a function with a `backend` argument"
+            )
         }
 
         // Make sure there's an existing body -- we're not making it from scratch.
-        guard let body = declaration.body else {
-            throw MacroError("@CastBackend macro expects a function with a body")
+        guard let body = decl.body else {
+            throw MacroError(
+                "@CastBackend macro expects a function with a body"
+            )
         }
 
         // If we were told that this function returns a widget, make sure
         // it actually has a return type.
         let widgetType: TypeSyntax?
         if
-            let args = Attribute(node).asMacroAttribute?.arguments,
+            let args = Attribute(attribute).asMacroAttribute?.arguments,
             let returnsWidgetExpr = args
                 .first(where: { $0.label == "returnsWidget" })?
                 .expr,
             returnsWidgetExpr.asBooleanLiteral?.value == true
         {
             guard let returnClause = signature.returnClause else {
-                throw MacroError("@CastBackend expects a return type when returnsWidget is true")
+                throw MacroError(
+                    "@CastBackend expects a return type when returnsWidget is true"
+                )
             }
             widgetType = returnClause.type
         } else {
             widgetType = nil
         }
 
-        // Get the name of the backend type via the parameter.
-        let oldBackendType = backendParameter.type
+        // MARK: Expansion
 
         // Set up identifiers.
-        let backendGenericName = context.makeUniqueName("NewBackend")
-        let innerFunctionName = context.makeUniqueName("castBackend")
-        let castedBackendName = context.makeUniqueName("castedBackend")
+        let backendGeneric = context.makeUniqueName("NewBackend")
+        let innerFunction = context.makeUniqueName("castBackend")
+        let castedBackend = context.makeUniqueName("castedBackend")
 
         // If we have a `widget` parameter, cast it.
-        let widgetCast: StmtSyntax =
-            if signature.parameterClause.parameters.hasParameter(named: "widget") {
-                "let widget = widget as! \(backendGenericName).Widget"
+        let widgetCast: StmtSyntax? =
+            if signature.parameterClause.parameters.contains(where: {
+                ($0.secondName ?? $0.firstName).trimmedDescription == "widget"
+            }) {
+                "let widget = widget as! \(backendGeneric).Widget"
             } else {
-                ""
+                nil
             }
 
         // If we're returning a widget, fix the return type from the inner
         // function and then cast the widget in the final return statement.
         let returnClause = if widgetType != nil {
-            ReturnClauseSyntax(type: TypeSyntax("\(backendGenericName).Widget"))
+            ReturnClauseSyntax(type: TypeSyntax("\(backendGeneric).Widget"))
         } else {
             signature.returnClause
         }
-        let returnExpression: ExprSyntax = if let widgetType {
-            "\(innerFunctionName)(\(castedBackendName)) as! \(widgetType)"
+        let returnCast = if let widgetType {
+            " as! \(widgetType)"
         } else {
-            "\(innerFunctionName)(\(castedBackendName))"
+            ""
         }
 
-        // Set up the inner function.
-        let castFunction = FunctionDeclSyntax(
-            name: innerFunctionName,
-            genericParameterClause: "<\(backendGenericName): AppBackend.Base & \(newBackendType)>",
-            signature: FunctionSignatureSyntax(
-                parameterClause: FunctionParameterClauseSyntax {
-                    "_ backend: \(backendGenericName)"
-                },
-                returnClause: returnClause
-            )
-        ) {
-            widgetCast
-            body.statements
-        }.formatted()
-
         return [
-            "\(castFunction)",
             """
-            guard let \(castedBackendName) = backend as? any AppBackend.Base & \(newBackendType) else {
-                fatalError("'\\(\(oldBackendType).self)' does not implement '\(newBackendType.trimmed)'")
+            func \(innerFunction)<
+                \(backendGeneric): AppBackend.Base & \(newBackend)
+            >(_ backend: \(backendGeneric))\(returnClause) {
+                \(widgetCast)
+                \(body.statements)
             }
-            """,
-            "return \(returnExpression)",
+            
+            guard let \(castedBackend) = backend as? any AppBackend.Base & \(newBackend) else {
+                fatalError("'\\(\(backendParameter.type).self)' does not implement '\(newBackend.trimmed)'")
+            }
+            return \(innerFunction)(\(castedBackend))\(raw: returnCast)
+            """
         ]
-    }
-}
-
-extension FunctionParameterListSyntax {
-    func hasParameter(named name: String) -> Bool {
-        contains { ($0.secondName ?? $0.firstName).trimmedDescription == name }
     }
 }
